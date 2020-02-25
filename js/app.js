6e07e7ac1348d3c25f95b8271fc6210ecaa88b50
Elizabeth-Murphy <61438150+Elizabeth-Murphy@users.noreply.github.com>
2020-02-25 10:47:39 +1000

    Added a Bootstrap Form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 
+			alert("i have no idea what i am doing")
+			console.log('always look on the bright side of life')
+
+			setTimeout(wakeUpUser, 5000);
+			function wakeUpUser() {
+				alert("hey man are you still there?")
+			}
